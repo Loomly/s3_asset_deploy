ccefef205f4c6bb401063acaa477d18c64f44927
Ari Summer <aribsummer@gmail.com>
2021-02-24 16:27:08 -0700

    Change RemovalManifest to perform put_object only if @manifest has changed [Issue #20] (#22)

--- spec/removal_manifest_spec.rb
+++ spec/removal_manifest_spec.rb
@@ -20,9 +20,65 @@ RSpec.describe S3AssetDeploy::RemovalManifest do
     end
   end
 
-  let(:s3_client_instance) { removal_manifest_s3_client.new }
+  let(:s3_client_instance) { s3_client.new }
 
   before { allow_any_instance_of(S3AssetDeploy::RemovalManifest).to receive(:s3) { s3_client_instance } }
+
+  describe "#changed?" do
+    let(:s3_client_instance) do
+      s3_client.new do
+        {
+          "assets/file-2-34567.jpg" => (Time.now - 172801).utc.iso8601,
+          "assets/file-3-9876666.jpg" => (Time.now - 172799).utc.iso8601
+        }
+      end
+    end
+
+    it "is false after initializing" do
+      expect(subject.changed?).to eq(false)
+    end
+
+    it "is true after setting an attribute" do
+      expect(subject.changed?).to eq(false)
+      subject.load
+      expect(subject.changed?).to eq(false)
+      subject["myfile"] = Time.now.utc.iso8601
+      expect(subject.changed?).to eq(true)
+    end
+
+    it "is true after deleting an attribute" do
+      expect(subject.changed?).to eq(false)
+      subject.load
+      expect(subject.changed?).to eq(false)
+      subject.delete("assets/file-2-34567.jpg")
+      expect(subject.changed?).to eq(true)
+    end
+
+    it "is false after saving" do
+      expect(subject.changed?).to eq(false)
+      subject.load
+      expect(subject.changed?).to eq(false)
+      subject.delete("assets/file-2-34567.jpg")
+      expect(subject.changed?).to eq(true)
+      subject.save
+      expect(subject.changed?).to eq(false)
+    end
+  end
+
+  describe "#save" do
+    it "does not make put_object request to S3 when manifest has not changed" do
+      subject.load
+      expect(s3_client_instance).to_not receive(:put_object)
+      expect(subject.save)
+    end
+
+    it "does makes put_object request to S3 when manifest has changed" do
+      subject.load
+      subject["myfile"] = Time.now.utc.iso8601
+      expect(s3_client_instance).to receive(:put_object)
+      expect(subject.save)
+    end
+  end
 
   describe "#load"  do
     let(:s3_client_instance) do
